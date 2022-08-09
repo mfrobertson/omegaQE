8bbc796a34ba8a491534f05fc1b4fc240e95b375
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2022-08-09 18:12:22 +0100

    Bias mpi script update

--- kkomega/scripts/bias_mpi.py
+++ kkomega/scripts/bias_mpi.py
@@ -4,6 +4,8 @@ from bias import Bias
 from cache.tools import parse_boolean
 import os
 import sys
+import datetime
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 
 def _get_workloads(N, world_size):
@@ -23,10 +25,18 @@ def _get_start_end(my_rank, workloads):
 
 def _get_Ls(N_Ls):
     samp1 = np.arange(30, 40, 5)
-    samp2 = np.logspace(1, 3, N_Ls-2) * 4
+    samp2 = np.logspace(1, 3, N_Ls - 2) * 4
     return np.concatenate((samp1, samp2))
 
-def _main(N_Ls, dir, bi_typ, gmv, fields):
+
+def _output(message, my_rank, _id):
+    if my_rank == 0:
+        f = open(f"_output/_bias_run_{_id}.out", "a")
+        f.write("[" + str(datetime.datetime.now()) + "] " + message + "\n")
+        f.close()
+
+
+def _main(N_Ls, dir, bi_typ, gmv, fields, _id):
     # get basic information about the MPI communicator
     world_comm = MPI.COMM_WORLD
     world_size = world_comm.Get_size()
@@ -34,13 +44,25 @@ def _main(N_Ls, dir, bi_typ, gmv, fields):
 
     start_time_tot = MPI.Wtime()
 
+    if my_rank == 0:
+        try:
+            os.remove("_bias_run.out")
+        except:
+            pass
+
+    _output("Setting up parallisation of workload.", my_rank, _id)
+
     Ls = _get_Ls(N_Ls)
 
     workloads = _get_workloads(N_Ls, world_size)
     my_start, my_end = _get_start_end(my_rank, workloads)
 
+    _output("Initialising Bias object.", my_rank, _id)
+
     N0_path = "cache/_N0"
     bias = Bias(N0_path, M_path="cache/_M", init_qe=False)
+
+    _output("Setting up cached Cls for bias initialisation.", my_rank, _id)
 
     parsed_fields_all = bias.qe.parse_fields()
     Cls = np.load("cache/_Cls/Cls_cmb_4000.npy")
@@ -51,67 +73,90 @@ def _main(N_Ls, dir, bi_typ, gmv, fields):
         bias.qe.initialise_manual(field, lenCl, gradCl, N)
     bias.qe.initialise()
 
+    _output("Initialisation finished.", my_rank, _id)
+
     nu = 353e9
 
-    if my_rank == 0:
-        bias.build_F_L(bi_typ, fields, gmv, nu)
-        Cl_kk = bias.cache.Cl_kk
-        Cov_kk = bias.cache.Cov_kk
-        Cl_gk = bias.cache.Cl_gk
-        Cl_Ik = bias.cache.Cl_Ik
-        sample_F_L_Ls = bias.cache.sample_F_L_Ls
-        F_L = bias.cache.F_L
-        C_inv = bias.cache.C_inv
-    else:
-        Cl_kk = np.empty(4000)
-        Cov_kk = np.empty(4000)
-        Cl_gk = np.empty(4000)
-        Cl_Ik = np.empty(4000)
-        sample_F_L_Ls = np.empty(300)
-        F_L = np.empty(300)
-        C_inv = np.empty((len(bi_typ),len(bi_typ),4000))
+    if bi_typ != "theory":
+        _output("Building F_L on root thread...", my_rank, _id)
+        if my_rank == 0:
+            bias.build_F_L(bi_typ, fields, gmv, nu)
+            Cl_kk = bias.cache.Cl_kk
+            Cov_kk = bias.cache.Cov_kk
+            Cl_gk = bias.cache.Cl_gk
+            Cl_Ik = bias.cache.Cl_Ik
+            sample_F_L_Ls = bias.cache.sample_F_L_Ls
+            F_L = bias.cache.F_L
+            C_inv = bias.cache.C_inv
+        else:
+            Cl_kk = np.empty(4001, dtype='d')
+            Cov_kk = np.empty(4001, dtype='d')
+            Cl_gk = np.empty(4001, dtype='d')
+            Cl_Ik = np.empty(4001, dtype='d')
+            sample_F_L_Ls = np.empty(300, dtype='d')
+            F_L = np.empty(300, dtype='d')
+            C_inv = np.empty((len(bi_typ), len(bi_typ), 4001), dtype='d')
 
-    world_comm.Bcast(Cl_kk, root=0)
-    world_comm.Bcast(Cov_kk, root=0)
-    world_comm.Bcast(Cl_gk, root=0)
-    world_comm.Bcast(Cl_Ik, root=0)
-    world_comm.Bcast(sample_F_L_Ls, root=0)
-    world_comm.Bcast(F_L, root=0)
-    world_comm.Bcast(C_inv, root=0)
+        _output("F_L build finished. Broadcasting...", my_rank, _id)
 
-    if my_rank != 0:
-        bias.cache.Cl_kk = Cl_kk
-        bias.cache.Cov_kk = Cov_kk
-        bias.cache.Cl_gk = Cl_gk
-        bias.cache.Cl_Ik = Cl_Ik
-        bias.cache.sample_F_L_Ls = sample_F_L_Ls
-        bias.cache.F_L = F_L
-        bias.cache.C_inv = C_inv
-        bias.cache.typs = bi_typ
-        bias.cache.fields = fields
-        bias.cache.gmv = gmv
-        bias.cache.nu = nu
+        world_comm.Bcast([Cl_kk, MPI.DOUBLE], root=0)
+        _output("  Cl_kk done.", my_rank, _id)
+        world_comm.Bcast([Cov_kk, MPI.DOUBLE], root=0)
+        _output("  Cov_kk done.", my_rank, _id)
+        world_comm.Bcast([Cl_gk, MPI.DOUBLE], root=0)
+        _output("  Cl_gk done.", my_rank, _id)
+        world_comm.Bcast([Cl_Ik, MPI.DOUBLE], root=0)
+        _output("  Cl_Ik done.", my_rank, _id)
+        world_comm.Bcast([sample_F_L_Ls, MPI.DOUBLE], root=0)
+        _output("  sample_F_L_Ls done.", my_rank, _id)
+        world_comm.Bcast([F_L, MPI.DOUBLE], root=0)
+        _output("  F_L done.", my_rank, _id)
+        world_comm.Bcast([C_inv, MPI.DOUBLE], root=0)
+        _output("  C_inv done.", my_rank, _id)
+
+        _output("Broadcasting finished. Setting up broadcasted F_Ls...", my_rank, _id)
+
+        if my_rank != 0:
+            bias.cache.Cl_kk = Cl_kk
+            bias.cache.Cov_kk = Cov_kk
+            bias.cache.Cl_gk = Cl_gk
+            bias.cache.Cl_Ik = Cl_Ik
+            bias.cache.sample_F_L_Ls = sample_F_L_Ls
+            bias.cache.F_L = F_L
+            bias.cache.F_L_spline = InterpolatedUnivariateSpline(sample_F_L_Ls, F_L)
+            bias.cache.C_inv = C_inv
+            bias.cache.typs = bi_typ
+            bias.cache.fields = fields
+            bias.cache.gmv = gmv
+            bias.cache.nu = nu
+
+    _output("Setup complete. Calculating bias...", my_rank, _id)
 
     start_time = MPI.Wtime()
-    N_A1_curl_TT, N_C1_curl_TT = bias.bias("TT", Ls[my_start: my_end], N_L1=10, N_L3=10, Ntheta12=10, Ntheta13=10)
+    N_A1_curl_TT, N_C1_curl_TT = bias.bias(bi_typ, fields, Ls[my_start: my_end], gmv=gmv, curl=True, N_L1=100, N_L3=100,Ntheta12=100, Ntheta13=100)
     end_time = MPI.Wtime()
 
+    _output("Bias calculation finished.", my_rank, _id)
 
     if my_rank == 0:
         print("Bias time: " + str(end_time - start_time))
+        _output("Bias time: " + str(end_time - start_time), my_rank, _id)
         N_arr = np.ones(N_Ls)
         N_arr[my_start: my_end] = N_A1_curl_TT + N_C1_curl_TT
         for rank in range(1, world_size):
             start, end = _get_start_end(rank, workloads)
-            N = np.empty(end-start)
+            N = np.empty(end - start)
             world_comm.Recv([N, MPI.DOUBLE], source=rank, tag=77)
             N_arr[start: end] = N
-        if not os.path.isdir(dir+"/tmp_run"):
-            os.mkdir(dir+"/tmp_run")
-        np.save(dir+"/tmp_run/Ls", Ls)
-        np.save(dir+"/tmp_run/N", N_arr)
+        gmv_str = "gmv" if gmv else "single"
+        dir += f"/{fields}_{gmv_str}/{bi_typ}"
+        if not os.path.isdir(dir):
+            os.makedirs(dir)
+        np.save(dir, Ls)
+        np.save(dir, N_arr)
         end_time_tot = MPI.Wtime()
         print("Total time: " + str(end_time_tot - start_time_tot))
+        _output("Total time: " + str(end_time_tot - start_time_tot), my_rank, _id)
     else:
         N = N_A1_curl_TT + N_C1_curl_TT
         world_comm.Send([N, MPI.DOUBLE], dest=0, tag=77)
@@ -119,11 +164,12 @@ def _main(N_Ls, dir, bi_typ, gmv, fields):
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if len(args) != 5:
-        raise ValueError("Must supply arguments: bi_typ fields gmv Nell dir")
+    if len(args) != 6:
+        raise ValueError("Must supply arguments: bi_typ fields gmv Nell dir id")
     bi_typ = str(args[0])
     fields = str(args[1])
     gmv = parse_boolean(args[2])
     N_Ls = int(args[3])
     dir = args[4]
-    _main(N_Ls, dir, bi_typ, gmv, fields)
+    _id = args[5]
+    _main(N_Ls, dir, bi_typ, gmv, fields, _id)
