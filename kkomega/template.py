e78d9d964ec208fcb6a25beff8e636912299c0ab
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2022-12-20 15:54:30 +0000

    Moving omegaQE class to template.py

--- kkomega/template.py
+++ kkomega/template.py
@@ -1,23 +1,22 @@
 import numpy as np
 from fields import Fields
-from fisher import Fisher
 from scipy.interpolate import InterpolatedUnivariateSpline
 import datetime
 
 
-class OmegaQE:
+class Template:
 
     def __init__(self, field_labels, exp="SO", N_pix=2 ** 7, Lmin=30, Lmax=3000, Lmax_map=None, kappa_map=None, F_L_spline=None, C_inv_spline=None):
-        self._fish = Fisher()
-        self._power = self._fish.power
-        self._cosmo = self._power.cosmo
         self.N_pix = N_pix
         self.Lmin = Lmin
         self.Lmax = Lmax
         self.Lmax_map = int(np.ceil(np.sqrt(2) * Lmax)) if Lmax_map is None else Lmax_map
         self.fields = Fields(field_labels, exp, self.N_pix, self.Lmax_map, kappa_map)
+        self._fish = self.fields.fish
+        self._power = self._fish.power
+        self._cosmo = self._power.cosmo
         self.L_map = self.fields.kM
-        self.Lx_map, self.Ly_map = self.get_Lx_Ly_maps()
+        self.Lx_map, self.Ly_map = self._get_Lx_Ly_maps()
         self.F_L_spline, self.C_inv_spline = self._get_F_L_and_C_inv_splines(F_L_spline, C_inv_spline)
         self.matter_PK = self._cosmo.get_matter_PK(typ="matter")
         self.a_bars = dict.fromkeys(self.fields.fields)
@@ -32,7 +31,7 @@ class OmegaQE:
             self.a_bars[field_i] = a_bar_i
 
     def _get_F_L_and_C_inv_splines(self, F_L_spline=None, C_inv_spline=None):
-        if F_L_spline != None and C_inv_spline != None:
+        if F_L_spline is not None and C_inv_spline is not None:
             return F_L_spline, C_inv_spline
         sample_Ls = self._fish.covariance.get_log_sample_Ls(Lmin=2, Lmax=self.Lmax_map, Nells=300)
         sample_Ls, F_L, C_inv = self._fish.get_F_L(self.fields.fields, Ls=sample_Ls, dL2=2, Ntheta=2000, nu=353e9, return_C_inv=True, Lmin=self.Lmin, Lmax=self.Lmax)
@@ -69,31 +68,31 @@ class OmegaQE:
             window = self._cosmo.cib_window_Chi(Chi, nu)
             return Cl, window
 
-    def get_window_k(self, Chi):
+    def _get_window_k(self, Chi):
         return self._cosmo.cmb_lens_window_matter(Chi, self._cosmo.get_chi_star(), False)
 
-    def get_Lx_Ly_maps(self):
+    def _get_Lx_Ly_maps(self):
         Lx, Ly = self.fields.get_kx_ky()
         Lx_map = Lx[None, :] * np.ones(np.shape(self.L_map))
         Ly_map = Ly[:, None] * np.ones(np.shape(self.L_map))
         return Lx_map, Ly_map
 
-    def L_comp_map(self, index):
+    def _L_comp_map(self, index):
         if index == 0:
             return self.Lx_map
         if index == 1:
             return self.Ly_map
 
-    def get_L_fac(self, p, r):
-        L_p = self.L_comp_map(p)
-        L_r = self.L_comp_map(r)
+    def _get_L_fac(self, p, r):
+        L_p = self._L_comp_map(p)
+        L_r = self._L_comp_map(r)
         L_map_inv = 1 / self.L_map
         L_map_inv[L_map_inv == np.inf] = 0
         return L_p * L_r * L_map_inv ** 2
 
-    def get_f_g(self, p, r, q, s, Cls, windows, matter_ps):
-        L_fac_f = self.get_L_fac(p, r)
-        L_fac_g = self.get_L_fac(q, s)
+    def _get_f_g(self, p, r, q, s, Cls, windows, matter_ps):
+        L_fac_f = self._get_L_fac(p, r)
+        L_fac_g = self._get_L_fac(q, s)
         h_f = 0
         h_g = 0
         for iii, field_i in enumerate(self.fields.fields):
@@ -126,14 +125,14 @@ class OmegaQE:
             I_tmp = np.zeros((np.shape(self.L_map)[0], np.shape(self.L_map)[0]), dtype="complex128")
             for p in range(2):
                 q = p
-                f_i, g_j = self.get_f_g(p, r, q, s, Cls, windows, matter_ps)
-                f_j, g_i = self.get_f_g(q, s, p, r, Cls, windows, matter_ps)
+                f_i, g_j = self._get_f_g(p, r, q, s, Cls, windows, matter_ps)
+                f_j, g_i = self._get_f_g(q, s, p, r, Cls, windows, matter_ps)
                 F_i = np.fft.irfft2(f_i, norm=norm)
                 G_j = np.fft.irfft2(g_j, norm=norm)
                 F_j = np.fft.irfft2(f_j, norm=norm)
                 G_i = np.fft.irfft2(g_i, norm=norm)
                 I_tmp += (F_i * G_j) - (F_j * G_i)
-            window_k = self.get_window_k(Chi)
+            window_k = self._get_window_k(Chi)
             I += 2 * np.fft.rfft2(I_tmp, norm=norm) / (Chi ** 2) * window_k
             print('\r', end='')
             print(f"[{str(datetime.datetime.now() - t0)[:-7]}] {int((Chi_i+1)/Nchi * 100)}%", end='')
