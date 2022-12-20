d0d5a30b2205b4684c1c39b30e3a8fc240095d15
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2022-12-20 15:59:14 +0000

    Removing degenerate F_L scripts and other minor changes

--- kkomega/cache/script_M.sh
+++ kkomega/cache/script_M.sh
@@ -1,7 +1,9 @@
 declare -a arr=("kk" "gg" "gk" "kg" "II" "Ik" "kI" "gI" "Ig")
 #declare -a arr=("II" "Ik" "kI" "gI" "Ig")
+#declare -a arr=("rr")
 for typ in "${arr[@]}"
 do
 echo "$typ"
 python M.py 5000 200 "True" "$typ"
+#python M.py 10000 200 "False" "$typ"
 done
