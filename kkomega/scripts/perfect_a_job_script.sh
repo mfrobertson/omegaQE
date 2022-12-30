ade5891253275a4e9314081258218448001a2972
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2022-12-30 19:03:19 +0000

    Example perfect_a jobscript

---
+++ kkomega/scripts/perfect_a_job_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --qos=debug
+#SBATCH --time=30
+#SBATCH --nodes=1
+#SBATCH --constraint=haswell
+
+module load python
+conda activate omegaqe
+cd $SCRATCH/repos/omegaQE/kkomega/
+
+EXP="SO_base"
+NBINS=2
+NELL=500
+DL2=1
+NTHETA=1000
+OUTDIR="$SCRATCH/perfect_a/results/$EXP"
+ID="TEST"
+srun -N 1 -n 3 -c 8 python _F_L_mpi.py "$EXP" "$NBINS" "$NELL" "$DL2" "$NTHETA" "$OUTDIR" "$ID"
