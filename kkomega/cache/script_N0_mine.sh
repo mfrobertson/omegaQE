b93c812cadfdb7e310699d11659678ba3a7ad8f0
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2023-01-27 15:23:20 +0000

    Update to N0 script

--- kkomega/cache/script_N0_mine.sh
+++ kkomega/cache/script_N0_mine.sh
@@ -1,14 +1,20 @@
-declare -a typs=("TT" "EE" "TE" "TB" "EB")
+#declare -a typs=("TT" "EE" "TE" "TB" "EB")
+declare -a typs=("TT")
 for typ in "${typs[@]}"
 do
 echo "$typ"
+python N0_mine.py SO_base "$typ" False gradient 1000 30 3000 30 5000
+python N0_mine.py SO_goal "$typ" False gradient 1000 30 3000 30 5000
 python N0_mine.py S4_base "$typ" False gradient 1000 30 3000 30 5000
 done
 
-declare -a gmv_typs=("TE" "EB" "TEB")
+#declare -a gmv_typs=("TE" "EB" "TEB")
+declare -a gmv_typs=("EB" "TEB")
 for typ in "${gmv_typs[@]}"
 do
 echo "$typ"
+python N0_mine.py SO_base "$typ" True gradient 500 30 3000 30 5000
+python N0_mine.py SO_goal "$typ" True gradient 500 30 3000 30 5000
 python N0_mine.py S4_base "$typ" True gradient 500 30 3000 30 5000
 done
 
