831414c91d72bf944d05d75073fc9f026d2e4396
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2023-01-31 16:37:11 +0000

    Allowing multiple sims per rank

--- kkomega/scripts/sim_bias_mpi.py
+++ kkomega/scripts/sim_bias_mpi.py
@@ -79,56 +79,66 @@ def _main(exp, typ, LDres, HDres, maps, gmv, Nsims, Lmin_cut, Lmax_cut, out_dir,
     _output(f"exp:{exp}, tracers:{typ}, LDres: {LDres}, HDres: {HDres}, fields:{maps}, gmv:{gmv}, Nsims: {Nsims}", my_rank, _id)
     nu = 353e9
 
-    _output("Initialising Fields object...", my_rank, _id)
-    if my_rank != 0:
-        time.sleep(120)
-    field_obj = Fields(typ, N_pix_pow=LDres, setup_cmb_lens_rec=True, HDres=HDres, Nsims=Nsims, sim=my_rank)
-
-    _output("Setting up noise...", my_rank, _id)
-    field_obj.setup_noise(exp=exp, qe=maps, gmv=gmv, ps="gradient", L_cuts=(30,3000,30,5000), iter=False, iter_ext=False, data_dir="data")
-
-    _output("    Preparing C_inv...", my_rank, _id)
-    Lmax_C_inv = 5000      # Highest L N0 calculated to
-    if my_rank == 0:
-        C_inv = field_obj.fish.covariance.get_C_inv(typ, Lmax_C_inv, nu)
-    else:
-        N_typs = np.size(list(typ))
-        C_inv = np.empty((N_typs, N_typs, Lmax_C_inv + 1), dtype='d')
-
-    _output("    Broadcasting and storing C_inv...", my_rank, _id)
-    world_comm.Bcast([C_inv, MPI.DOUBLE], root=0)
-
-    _output("    Splining C_inv...", my_rank, _id)
-    C_inv_splines = _C_inv_splines(typ, C_inv, Lmax_C_inv, Lmin_cut, Lmax_cut)
-
-    _output("    Splining cached F_L...", my_rank, _id)
-    Ls, F_L = _F_L(typ, exp, gmv, maps)
-    F_L_spline = InterpolatedUnivariateSpline(Ls, F_L)
-
     _output("Starting sim bias calculation...", my_rank, _id)
-    qe_typ = _qe_typ(maps, gmv)
-    start_time = MPI.Wtime()
-    omega_rec = field_obj.get_omega_rec(qe_typ, include_noise=False)
-    end_time = MPI.Wtime()
-    _output("Lensing reconstruction time: " + str(end_time - start_time), my_rank, _id)
 
-    start_time = MPI.Wtime()
-    omega_temp = field_obj.get_omega_template(Nchi=100, F_L_spline=F_L_spline, C_inv_spline=C_inv_splines)
-    end_time = MPI.Wtime()
-    _output("Template construction time: " + str(end_time - start_time), my_rank, _id)
+    workloads = _get_workloads(Nsims, world_size)
+    my_start, my_end = _get_start_end(my_rank, workloads)
 
-    _output("Calculating cross-spectrum", my_rank, _id)
-    Ls, ps_tmp = field_obj.get_ps(omega_rec, omega_temp, kmin=Lmin_cut, kmax=Lmax_cut)
+    ps_arr = None
+
+    Lmax_C_inv = 5000  # Highest L N0 calculated to
+    N_typs = np.size(list(typ))
+    C_inv = np.empty((N_typs, N_typs, Lmax_C_inv + 1), dtype='d')
+
+    for sim in np.arange(my_start, my_end):
+        _output("Initialising Fields object...", my_rank, _id)
+        if my_rank != 0:
+            time.sleep(120)
+        field_obj = Fields(typ, N_pix_pow=LDres, setup_cmb_lens_rec=True, HDres=HDres, Nsims=Nsims, sim=my_rank)
+
+        if my_rank == 0 and sim == 0:
+            _output("    Preparing C_inv...", my_rank, _id)
+            C_inv = field_obj.fish.covariance.get_C_inv(typ, Lmax_C_inv, nu)
+
+            _output("    Broadcasting and storing C_inv...", my_rank, _id)
+            world_comm.Bcast([C_inv, MPI.DOUBLE], root=0)
+
+        C_inv_splines = _C_inv_splines(typ, C_inv, Lmax_C_inv, Lmin_cut, Lmax_cut)
+
+        Ls, F_L = _F_L(typ, exp, gmv, maps)
+        F_L_spline = InterpolatedUnivariateSpline(Ls, F_L)
+
+        _output("Setting up noise...", my_rank, _id)
+        field_obj.setup_noise(exp=exp, qe=maps, gmv=gmv, ps="gradient", L_cuts=(30, 3000, 30, 5000), iter=False, iter_ext=False, data_dir="data")
+
+        qe_typ = _qe_typ(maps, gmv)
+        start_time = MPI.Wtime()
+        omega_rec = field_obj.get_omega_rec(qe_typ, include_noise=False)
+        end_time = MPI.Wtime()
+        _output("Lensing reconstruction time: " + str(end_time - start_time), my_rank, _id)
+
+        start_time = MPI.Wtime()
+        omega_temp = field_obj.get_omega_template(Nchi=100, F_L_spline=F_L_spline, C_inv_spline=C_inv_splines)
+        end_time = MPI.Wtime()
+        _output("Template construction time: " + str(end_time - start_time), my_rank, _id)
+
+        _output("Calculating cross-spectrum", my_rank, _id)
+        Ls, ps_tmp = field_obj.get_ps(omega_rec, omega_temp, kmin=Lmin_cut, kmax=Lmax_cut)
+
+        if ps_arr is None:
+            ps_arr = np.zeros((my_end-my_start, np.size(ps_tmp)))
+        ps_arr[sim] = ps_tmp
 
     _output("Broadcasting results...", my_rank, _id)
 
     if my_rank == 0:
-        ps_all = np.zeros((Nsims, np.size(ps_tmp)))
-        ps_all[0] = ps_tmp
+        ps_all = np.zeros((Nsims, np.size(ps_arr[0])))
+        ps_all[my_start:my_end] = ps_arr
         for rank in range(1, world_size):
-            ps_tmp = np.empty(np.shape(ps_tmp))
+            start, end = _get_start_end(rank, workloads)
+            ps_tmp = np.empty((end-start, np.size(ps_all[0])))
             world_comm.Recv([ps_tmp, MPI.DOUBLE], source=rank, tag=77)
-            ps_all[rank] = ps_tmp
+            ps_all[start:end] = ps_tmp
         gmv_str = "gmv" if gmv else "single"
         out_dir += f"/{typ}/{exp}/{gmv_str}/{maps}/{LDres}_{HDres}/{Lmin_cut}_{Lmax_cut}/{Nsims}/"
         if not os.path.isdir(out_dir):
@@ -139,7 +149,7 @@ def _main(exp, typ, LDres, HDres, maps, gmv, Nsims, Lmin_cut, Lmax_cut, out_dir,
         print("Total time: " + str(end_time_tot - start_time_tot))
         _output("Total time: " + str(end_time_tot - start_time_tot), my_rank, _id)
     else:
-        world_comm.Send([ps_tmp, MPI.DOUBLE], dest=0, tag=77)
+        world_comm.Send([ps_arr, MPI.DOUBLE], dest=0, tag=77)
 
 
 if __name__ == '__main__':
