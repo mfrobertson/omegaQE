86fe1f63ea21b50ed7d76c4f90f24b8a35648679
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2023-03-08 13:41:18 +0000

    Zero omega spectrum input for lensit

--- kkomega/scripts/lensit_Cl_mod.py
+++ kkomega/scripts/lensit_Cl_mod.py
@@ -10,6 +10,9 @@ from powerspectra import Powerspectra
 def _get_Cl_pp(ells):
     return Powerspectra().get_kappa_ps(ells) * 4 / (2*np.pi)
 
+def _get_Cl_oo(ells):
+    return np.zeros(np.size(ells))
+
 
 def main():
     path_to_lensit = f'../../../Lensit'
@@ -17,7 +20,12 @@ def main():
     ells = cols[0]
     Cl_pp = _get_Cl_pp(ells)
     cols[5] = Cl_pp
-    # np.savetxt(f'my_fiducial_flatsky_lenspotentialCls.dat', cols.transpose(), fmt='%.5e')    #File to be added to /lensit/data/cls/fiducial_flatsky_lenspotentialCls.dat
+    # np.savetxt(f'my_fiducial_flatsky_lenspotentialCls.dat', cols.transpose(), fmt='%.5e')    #File to be added to /lensit/data/cls/
+    cols = np.loadtxt(f'{path_to_lensit}/lensit/data/cls/fiducial_fieldrotationCls.dat').transpose()
+    ells = np.arange(np.size(cols[0]))
+    cols[0] = _get_Cl_oo(ells)
+    np.savetxt(f'my_fiducial_fieldrotationCls.dat', cols.transpose(), fmt='%.5e')    #File to be added to /lensit/data/cls/
+
 
 
 if __name__ == '__main__':
