8854d44c46226c55e387bca508aea4ecb1e09164
Mathew Robertson <mr671@mps023637.phys.susx.ac.uk>
2023-04-14 13:48:33 +0100

    Adding __init__ files

---
+++ omegaqe/__init__.py
@@ -0,0 +1,8 @@
+import os
+import omegaqe.tools as tools
+
+sep = tools.getFileSep()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+CACHE_DIR = dir_path + f"{sep}cache{sep}"
+DATA_DIR = dir_path + f"{sep}data{sep}"
+RESULTS_DIR = dir_path + f"{sep}results{sep}"
