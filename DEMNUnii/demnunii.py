3db65f165e5356479e97dc3d55e5414b6c70477f
Mathew Robertson <mr671@sussex.ac.uk>
2023-05-16 19:47:32 +0100

    demnunii updates

--- DEMNUnii/demnunii.py
+++ DEMNUnii/demnunii.py
@@ -1,4 +1,5 @@
 from omegaqe.tools import getFileSep
+from omegaqe.cosmology import Cosmology
 import numpy as np
 from astropy.io import fits
 import healpy as hp
@@ -6,13 +7,17 @@ from configparser import ConfigParser
 import pandas as pd
 
 
+DATA_DIR = "/mnt/lustre/users/astro/mr671/DEMNUnii/LCDM/"
+
+
 class Demnunii:
 
     def __init__(self):
-        self.data_dir = "/mnt/lustre/users/astro/mr671/DEMNUnii/LCDM/"
+        self.data_dir = DATA_DIR
         self.config = self.setup_config()
         self.nside = 4096
         self.snap_df = self.get_snap_info()
+        self.cosmo = Cosmology("DEMNUnii_params.ini")
 
     def setup_config(self):
         sep = getFileSep()
@@ -49,12 +54,12 @@ class Demnunii:
     def get_snaps_z(self, zmin, zmax):
         zs = np.asarray(self.snap_df["6:zstart"])
         snaps = np.asarray(self.snap_df["#1:output"])
-        return snaps[np.where(np.logical_and(zs > zmin, zs < zmax))]
+        return snaps[np.where(np.logical_and(zs >= zmin, zs <= zmax))]
 
     def get_snaps_chi(self, chi_min, chi_max):
         chis = np.asarray(self.snap_df["4:xstart"])
         snaps = np.asarray(self.snap_df["#1:output"])
-        return snaps[np.where(np.logical_and(chis > chi_min, chis < chi_max))]
+        return snaps[np.where(np.logical_and(chis >= chi_min, chis <= chi_max))]
 
     def get_density_map(self, zmin=0, zmax=1100, chi_min=None, chi_max=None, use_chi=False):
         pixel_area = hp.pixelfunc.nside2pixarea(self.nside)
@@ -66,6 +71,51 @@ class Demnunii:
             rho += self.get_snap(snap) * particle_mass / pixel_area
         rho_bar = np.mean(rho)
         return rho / rho_bar - 1
+
+    # def _get_z(self, snap):
+    #     return np.asarray(self.snap_df["2:z"][self.snap_df["#1:output"] == snap])[0]
+    #
+    # def _get_dChi(self, snap):
+    #     return np.asarray(self.snap_df["8:dx"][self.snap_df["#1:output"] == snap])[0]
+    #
+    # def _bias(self, snap, typ="unity"):
+    #     if typ == "unity":
+    #         return 1
+    #     if typ == "LSST":
+    #         return 1 + (0.84 * self._get_z(snap))
+    #
+    # def get_gal_map(self, zmin=0, zmax=1100, bias="unity", chi_min=None, chi_max=None, use_chi=False):
+    #     pixel_area = hp.pixelfunc.nside2pixarea(self.nside)
+    #     npix = hp.nside2npix(self.nside)
+    #     particle_mass = self.get_particle_mass()
+    #     gal = np.zeros(npix)
+    #     snaps = self.get_snaps_chi(chi_min, chi_max) if use_chi else self.get_snaps_z(zmin, zmax)
+    #     for snap in snaps:
+    #         gal += self._bias(snap, bias) * self.get_snap(snap) * particle_mass / pixel_area
+    #     gal_bar = np.mean(gal)
+    #     return gal / gal_bar - 1
+    #
+    # def _window(self, z, typ):
+    #     if typ == "LSST":
+    #         z0 = 0.311
+    #         return 1/(2*z0) * (z/z0)**2 * np.exp(-z/z0) * self.cosmo.get_hubble(z)
+    #
+    # def _get_Ng(self, zmin=0, zmax=1100, window="LSST"):
+    #     zs = np.linspace(zmin, zmax, 1000)
+    #     dz = zs[1] - zs[0]
+    #     return np.sum(self._window(zs, window)) * dz
+    #
+    # def get_obs_gal_map(self, zmin=0, zmax=1100, bias="LSST", window="LSST", chi_min=None, chi_max=None, use_chi=False):
+    #     pixel_area = hp.pixelfunc.nside2pixarea(self.nside)
+    #     npix = hp.nside2npix(self.nside)
+    #     particle_mass = self.get_particle_mass()
+    #     gal = np.zeros(npix)
+    #     snaps = self.get_snaps_chi(chi_min, chi_max) if use_chi else self.get_snaps_z(zmin, zmax)
+    #     for snap in snaps:
+    #         z = self._get_z(snap)
+    #         gal += self._bias(snap, bias) * self._window(z, window) * self.get_snap(snap) * particle_mass / pixel_area
+    #     gal_bar = np.mean(gal)
+    #     return gal / gal_bar - 1
 
     def get_ps(self, map, lmax=4000):
         return hp.sphtfunc.anafast(map, lmax=lmax, use_pixel_weights=True)
