e932e1c9f721cf282e7e212891f7007e9173f44a
Mathew Robertson <mr671@sussex.ac.uk>
2023-05-18 20:31:11 +0100

    Adding extra cmb sims with gaussian alpha + unl Tmap save

--- DEMNUnii/len_cmbs.py
+++ DEMNUnii/len_cmbs.py
@@ -1,28 +1,53 @@
 import lenspyx
-from lenspyx.utils_hp import synalm
+from lenspyx.utils_hp import synalm, almxfl
+from omegaqe.powerspectra import Powerspectra
 from omegaqe.cosmology import Cosmology
 from omegaqe.tools import getFileSep
 from demnunii import Demnunii
+from scipy.interpolate import InterpolatedUnivariateSpline
 import healpy as hp
 import numpy as np
 import sys
 import os
 
+power = Powerspectra()
 cosmo = Cosmology("DEMNUnii_params.ini")
+power.cosmo = cosmo
 demnunii = Demnunii()
+sep = getFileSep()
 
 
-def get_deflection_fields(lmax):
+def get_deflection_fields_demnunii(lmax):
     kappa_map = demnunii.get_kappa_map()
     omega_map = demnunii.get_omega_map()
     klm = hp.sphtfunc.map2alm(kappa_map, lmax, lmax, use_pixel_weights=True)
     olm = hp.sphtfunc.map2alm(omega_map, lmax, lmax, use_pixel_weights=True)
 
-    ells = np.arange(lmax + 1)
-    fac = 2 / ells / (ells + 1)
-    fac[0] = 0
+    ells = np.arange(1, lmax + 1)
+    fac = np.zeros(np.size(ells) + 1)
+    fac[1:] = 2 / ells / (ells + 1)
     plm = hp.sphtfunc.almxfl(klm, fac, None, False)
     Olm = hp.sphtfunc.almxfl(olm, fac, None, False)
+
+    glm = hp.sphtfunc.almxfl(plm, np.sqrt(np.arange(lmax + 1, dtype=float) * np.arange(1, lmax + 2)), None, False)
+    clm = hp.sphtfunc.almxfl(Olm, np.sqrt(np.arange(lmax + 1, dtype=float) * np.arange(1, lmax + 2)), None, False)
+
+    return glm, clm
+
+
+def get_deflection_fields_lenspyx(lmax, omega_acc=4):
+    ells = np.arange(1, lmax + 1)
+    Cl_phi = np.zeros(np.size(ells) + 1)
+    Cl_phi[1:] = power.get_phi_ps(ells)
+    plm = synalm(Cl_phi, lmax=lmax, mmax=lmax)
+
+    ells_omega, omega_ps = cosmo.get_postborn_omega_ps(acc=omega_acc)
+    Cl_omega = np.zeros(np.size(ells) + 1)
+    Cl_omega[1:] = InterpolatedUnivariateSpline(ells_omega, omega_ps)(ells)
+    olm = synalm(Cl_omega, lmax=lmax, mmax=lmax)
+    fac = np.zeros(np.size(ells) + 1)
+    fac[1:] = 2 / ells / (ells + 1)
+    Olm = almxfl(olm, fac, None, False)
 
     glm = hp.sphtfunc.almxfl(plm, np.sqrt(np.arange(lmax + 1, dtype=float) * np.arange(1, lmax + 2)), None, False)
     clm = hp.sphtfunc.almxfl(Olm, np.sqrt(np.arange(lmax + 1, dtype=float) * np.arange(1, lmax + 2)), None, False)
@@ -59,23 +84,39 @@ def get_lensed_maps(dlm, unl_alms, nthreads):
     return Tlen, Qlen, Ulen
 
 
-def save_maps(loc, len_maps, sim):
-    sep = getFileSep()
-    directory = f"{loc}{sep}{sim}"
+def get_unlensed_Tmap(unl_alms, lmax, nthreads):
+    Tlm_unl = unl_alms[0]
+    return lenspyx.get_geom(('healpix', {'nside': demnunii.nside})).alm2map(Tlm_unl, lmax, lmax, nthreads=nthreads)
+
+
+def save_lens_maps(loc, len_maps, sim):
+    directory = f"{loc}"
     if not os.path.isdir(directory):
         os.makedirs(directory)
-    hp.fitsfunc.write_map(f"{directory}{sep}TQU", len_maps)
+    hp.fitsfunc.write_map(f"{directory}{sep}TQU_{sim}.fits", len_maps)
+
+
+def save_Tunl(loc, Tmap, sim):
+    directory = f"{loc}"
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    hp.fitsfunc.write_map(f"{directory}{sep}T_{sim}.fits", Tmap)
 
 
 def main(lmax, nsims, nthreads, loc):
-    glm, clm = get_deflection_fields(lmax)
-    dlm = np.array([glm, clm])
-    Tlm_unl, Elm_unl, Blm_unl = get_unlensed_cmb_ps()
-    unl_cmb_spectra = np.array([Tlm_unl, Elm_unl, Blm_unl])
+    glm, clm = get_deflection_fields_demnunii(lmax)
+    dlm_dem = np.array([glm, clm])
+    glm, clm = get_deflection_fields_lenspyx(lmax)
+    dlm_diff_alpha = np.array([glm, clm])
+    Cl_TT_unl, Cl_EE_unl, Cl_BB_unl = get_unlensed_cmb_ps()
+    unl_cmb_spectra = np.array([Cl_TT_unl, Cl_EE_unl, Cl_BB_unl])
     for sim in range(nsims):
         unl_alms = get_unlensed_alms(lmax, unl_cmb_spectra)
-        len_maps = get_lensed_maps(dlm, unl_alms, nthreads)
-        save_maps(loc, len_maps, sim)
+        len_maps_dem = get_lensed_maps(dlm_dem, unl_alms, nthreads)
+        save_lens_maps(f"{loc}{sep}demnunii", len_maps_dem, sim)
+        len_maps_diff_phi = get_lensed_maps(dlm_diff_alpha, unl_alms, nthreads)
+        save_lens_maps(f"{loc}{sep}diff_alpha", len_maps_diff_phi, sim)
+        save_Tunl(f"{loc}{sep}T_unl", get_unlensed_Tmap(unl_alms, lmax, nthreads), sim)
 
 
 if __name__ == '__main__':
