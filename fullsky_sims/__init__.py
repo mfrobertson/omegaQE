9b346849b3427679426c0c90b62db29b3017accc
Mathew Robertson <mr671@sussex.ac.uk>
2023-11-29 16:49:46 +0000

    Fullsky init

---
+++ fullsky_sims/__init__.py
@@ -0,0 +1,4 @@
+DATA_DIR = "/mnt/lustre/users/astro/mr671/DEMNUnii/LCDM/"
+CACHE_DIR = "/mnt/lustre/users/astro/mr671/omegaQE/fullsky_sims/cache/"
+SIMS_DIR = "/mnt/lustre/users/astro/mr671/len_cmbs/sims"
+LMAX_MAP = 5000
