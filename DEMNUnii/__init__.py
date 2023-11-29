4ba7dedd363a6886955f4fc25d3ff5ffb97d286a
Mathew Robertson <mr671@sussex.ac.uk>
2023-11-29 16:48:12 +0000

    Refactoring

--- DEMNUnii/__init__.py
+++
@@ -1,4 +0,0 @@
-DATA_DIR = "/mnt/lustre/users/astro/mr671/DEMNUnii/LCDM/"
-CACHE_DIR = "/mnt/lustre/users/astro/mr671/omegaQE/DEMNUnii/cache/"
-SIMS_DIR = "/mnt/lustre/users/astro/mr671/len_cmbs/sims"
-LMAX_MAP = 5000
