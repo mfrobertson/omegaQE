aba0f923532f9ab47ec67212ec8f7c7228cdcc47
Mathew Robertson <mr671@sussex.ac.uk>
2024-01-04 19:52:44 +0000

    Minor

--- fullsky_sims/scripts/lss_cls.py
+++ fullsky_sims/scripts/lss_cls.py
@@ -7,12 +7,12 @@ def main():
     nbins = 150
     fields = Fields("SO_base", use_lss_cache=True, use_cmb_cache=True, nthreads=10)
     Cl_kk = fields.get_Cl("kk")
-    Cl_kk = fields.dm.sht.smoothed_cl(Cl_kk, nbins, False)
+    Cl_kk = fields.nbody.sht.smoothed_cl(Cl_kk, nbins, False)
     Cl_kg = fields.get_Cl("kg", smoothing_nbins=nbins)
     Cl_kI = fields.get_Cl("kI", smoothing_nbins=nbins)
-    np.save(f"{fullsky_sims.CACHE_DIR}/_lss_cls/cl_kk.npy", Cl_kk)
-    np.save(f"{fullsky_sims.CACHE_DIR}/_lss_cls/cl_gk.npy", Cl_kg)
-    np.save(f"{fullsky_sims.CACHE_DIR}/_lss_cls/cl_Ik.npy", Cl_kI)
+    np.save(f"{fields.nbody.cache_dir}/_lss_cls/cl_kk.npy", Cl_kk)
+    np.save(f"{fields.nbody.cache_dir}/_lss_cls/cl_gk.npy", Cl_kg)
+    np.save(f"{fields.nbody.cache_dir}/_lss_cls/cl_Ik.npy", Cl_kI)
 
 
 if __name__ == '__main__':
