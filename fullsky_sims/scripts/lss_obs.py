4333089f67ad7cd591392977a5ae440346d9c98f
Mathew Robertson <mr671@sussex.ac.uk>
2024-01-04 19:53:25 +0000

    Updates to lss_obs

--- fullsky_sims/scripts/lss_obs.py
+++ fullsky_sims/scripts/lss_obs.py
@@ -1,22 +1,16 @@
-from fullsky_sims.demnunii import Demnunii
+import fullsky_sims
 
 
-def main(nthreads=30):
-    dm = Demnunii(nthreads)
-    kappa_map = dm.get_kappa_map(pb=True)
-    omega_map = dm.get_omega_map()
-    gal_map = dm.get_obs_gal_map(verbose=True)
-    cib_map = dm.get_obs_cib_map(verbose=True)
+def main(nbody="DEMNUnii", nthreads=30):
+    nbody = fullsky_sims.wrapper_class(nbody, nthreads)
+    kappa_map = nbody.get_kappa_map(pb=True)
 
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/k.fits", kappa_map)
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/w.fits", omega_map)
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/g.fits", gal_map)
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/I.fits", cib_map)
+    nbody.sht.write_map(f"{nbody.cache_dir}/_maps/k.fits", kappa_map)
 
-    gal_map = dm.get_obs_gal_map(verbose=True, lensed=True)
-    cib_map = dm.get_obs_cib_map(verbose=True, lensed=True)
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/g_len.fits", gal_map)
-    dm.sht.write_map(f"{dm.cache_dir}/_maps/I_len.fits", cib_map)
+    gal_map = nbody.get_obs_gal_map(verbose=True, lensed=True)
+    cib_map = nbody.get_obs_cib_map(verbose=True, lensed=True)
+    nbody.sht.write_map(f"{nbody.cache_dir}/_maps/g_len.fits", gal_map)
+    nbody.sht.write_map(f"{nbody.cache_dir}/_maps/I_len.fits", cib_map)
 
 
 if __name__ == '__main__':
