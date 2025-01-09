fixing omega bi sign to clockwise

omegaqe/bb_rot.py
@@ -41,7 +41,7 @@ def _BB_mixed(Ls, verbose, N_L1, N_L2, Ntheta1, Ntheta2):
                 w3[(L1p_vec+L2_vec).rho < Lmin] = 0
                 theta1p2 = L1p_vec.deltaphi(L2_vec)
                 L_fac = (L1p**4 * L2**2) + (2*L1p**3*L2**3) + (L1p**2 * L2**4)   #Depending on convention for omega...
-                bi = 8 * fish.bi.get_bispectrum("kkw", L1p, L2, theta=theta1p2, M_spline=True)/(-L_fac) #Lfac may be -ve
+                bi = 8 * fish.bi.get_bispectrum("kkw", L1p, L2, theta=theta1p2, M_spline=True)/(L_fac) #Lfac may be -ve
                 I_theta2 = w2 * w3 * (L1_vec @ L2_vec) * (L1*L2p*np.sin(L2p_vec.deltaphi(L1_vec))) * bi
 
                 I_L2[kkk] = InterpolatedUnivariateSpline(thetas2, I_theta2).integral(0, 2*np.pi-dTheta2)

omegaqe/bispectra.py
@@ -185,7 +185,7 @@ class Bispectra:
 
     def _omega_bispectrum_angle(self, typ, L1, L2, theta12, M_spline, zmin, zmax, nu, gal_bins, gal_distro="LSST_gold"):
         M1, M2, _ = self._bispectra_prep(typ, L1, L2, None, M_spline, zmin, zmax, nu=nu, gal_bins=gal_bins, gal_distro=gal_distro)
-        return -np.sin(2 * theta12) * (M1 - M2)    #Using anti-clockwise omega so bi has different sign compared to Pratten & Lewis
+        return np.sin(2 * theta12) * (M1 - M2)    #Using clockwise omega convention of Pratten & Lewis
 
     def _get_F2(self, L1, L2, L3):
         A = 1
@@ -229,7 +229,7 @@ class Bispectra:
 
     def get_lss_bispectrum(self, typ, L1, L2, L3=None, theta=None, zmin=0, zmax=None, nu=353e9, gal_bins=(None,None,None,None), gal_distro="LSST_gold"):
         if "w" in typ:
-            raise ValueError(f"Bispectrum type {typ} not got lss bispectrum")
+            return 0
         if L3 is None:
             return self.get_lss_bispectrum(typ, L1, L2, self._get_third_L(L1, L2, theta), None, zmin, zmax, nu, gal_bins, gal_distro)
         Nchi = 100